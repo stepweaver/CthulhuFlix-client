677a93fcc63aeaaae4a4f29c1d11b1380b504a0d
Stephen Weaver <sweaver1@ND.EDU>
2023-07-07 14:04:37 -0400

    Update main-view.jsx
    Import new components and expand movies data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,61 @@
 import { useState } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
   const [movies, setMovies] = useState([
-    { id: 1, title: "Eloquent JavaScript" },
-    { id: 2, title: "Mastering JavaScript Functional Programming" },
-    { id: 3, title: "JavaScript: The Good Parts" },
-    { id: 4, title: "JavaScript: The Definitive Guide" },
-    { id: 5, title: "The Road to React" }
+    {
+      id: 1,
+      title: "Eloquent JavaScript",
+      imageURL:
+        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+      author: "Marijn Haverbeke"
+    },
+    {
+      id: 2,
+      title: "Mastering JavaScript Functional Programming",
+      imageURL:
+        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      author: "Federico Kereki"
+    },
+    {
+      id: 3,
+      title: "JavaScript: The Good Parts",
+      imageURL:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
+      author: "Douglas Crockford"
+    },
+    {
+      id: 4,
+      title: "JavaScript: The Definitive Guide",
+      imageURL:
+        "https://images-na.ssl-images-amazon.com/images/I/51HbNW6RzhL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+      author: "David Flanagan"
+    },
+    {
+      id: 5,
+      title: "The Road to React",
+      imageURL:
+        "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
+      author: "Robin Wieruch"
+    }
   ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} />;
+  }
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
-  } else {
-    return (
-      <div>
-        {movies.map((movie) => {
-          return <div key={movie.id}>{movie.title}</div>;
-        })}
-      </div>
-    )
   }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 };
