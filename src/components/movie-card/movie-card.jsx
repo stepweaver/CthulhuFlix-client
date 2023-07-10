a9fe87a22f9eaf88f03e1b77c3964189d874249f
Stephen Weaver <sweaver1@ND.EDU>
2023-07-10 09:55:15 -0400

    Update movie-card.jsx
    PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,25 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    releaseYear: PropTypes.string.isRequired,
+    Rating: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      yearOfBirth: PropTypes.string.isRequired,
+    }).isRequired,
+    imageURL: PropTypes.string.isRequired,
+    Actors: PropTypes.arrayOf(PropTypes.string).isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
