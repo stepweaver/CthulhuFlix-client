1472ee936cad2440c1b63ff02106d5f0b57739fa
Stephen Weaver <sweaver1@ND.EDU>
2023-07-10 13:34:52 -0400

    Update main-view.jsx
    Import and render LoginView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     fetch('https://cthulhuflix-2f8f4cc270b5.herokuapp.com/movies')
@@ -33,6 +35,10 @@ export const MainView = () => {
         setMovies(moviesFromApi);
       });
   }, []);
+
+  if (!user) {
+    return <LoginView />;
+  }
 
 
   if (selectedMovie) {
