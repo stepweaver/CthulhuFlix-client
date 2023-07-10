f6456871223cbd39946f50c9d508d3bdc595c4e8
Stephen Weaver <sweaver1@ND.EDU>
2023-07-10 10:43:51 -0400

    Update movie-view.jsx
    Basic MovieView layout

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,15 +2,21 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img 
+          className='movie-poster'
+          src={movie.imageURL}
+          alt={`Poster for ${movie.Title}`}
+        />
       </div>
       <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Author: </span>
-        <span>{movie.author}</span>
+        <h2 className='movie-title'>{movie.Title}</h2>
+        <h5>
+          {movie.releaseYear} | {movie.Genre?.Name}
+        </h5>
+        <hr />
+        <p>{movie.Description}</p>
+        <p>Actors: {movie.Actors.join(', ')}</p>
+        <p>Director: {movie.Director?.Name}</p>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
