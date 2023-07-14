685bfbfc8742e4d961fb2da021680a947b5925d9
Stephen Weaver <sweaver1@ND.EDU>
2023-07-14 01:22:11 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -89,7 +89,7 @@ export const MainView = () => {
       ) : (
         <>
           {movies.map((movie) => (
-            <Col key={movie.id} md={3}>
+            <Col className='mb-5' key={movie.id} md={3}>
               <MovieCard
                 movie={movie}
                 onMovieClick={(newSelectedMovie) => {
