2ed5b30fda2f67c15f9caa7af68927b783b400f5
Stephen Weaver <sweaver1@ND.EDU>
2023-07-16 07:30:38 -0400

    Update signup-view.jsx

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 
 export const SignupView = () => {
   const [username, setUsername] = useState('');
@@ -35,54 +35,60 @@ export const SignupView = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className='mb-3' controlId='signupUsername'>
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength='3'
-          placeholder='Username'
-          />
-      </Form.Group>
-      <Form.Group className='mb-3' controlId='signupPassword'>
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          minLength='6'
-          placeholder='Password'
-        />
-      </Form.Group>
-      <Form.Group className='mb-3' controlId='signupEmail'>
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder='Enter email'
-        />
-        <Form.Text className='text-muted'>
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group className='mb-3' controlId='signupBirthday'>
-        <Form.Label>Birthday</Form.Label>
-        <Form.Control
-          type='date'
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Button variant='primary' type='submit'>
-        Submit
-      </Button>
-    </Form>
+    <Container>
+      <Row>
+        <Col>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className='mb-3' controlId='signupUsername'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type='text'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                minLength='3'
+                placeholder='Username'
+                />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='signupPassword'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                minLength='6'
+                placeholder='Password'
+              />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='signupEmail'>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder='Enter email'
+              />
+              <Form.Text className='text-muted'>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='signupBirthday'>
+              <Form.Label>Birthday</Form.Label>
+              <Form.Control
+                type='date'
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
