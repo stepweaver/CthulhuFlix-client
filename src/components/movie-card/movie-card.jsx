3f7f1710394a1896885b62ff0aca6c7cf369bdaa
Stephen Weaver <sweaver1@ND.EDU>
2023-07-18 09:54:39 -0400

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,7 +7,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Img variant='top' src={movie.imageURL} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Card.Text>{movie.releaseYear}</Card.Text>
       </Card.Body>
     </Card>
   );
