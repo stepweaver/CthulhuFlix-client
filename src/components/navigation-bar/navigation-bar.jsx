148c38264d6d22aacea8a988523c57f4d13cf22f
Stephen Weaver <sweaver1@ND.EDU>
2023-07-21 11:17:49 -0400

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-bootstrap';
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
-    <Navbar bg='dark' expand='lg'>
-      <Container>
+    <Navbar bg='transparent' expand='lg'>
+      <Container fluid>
         <Navbar.Brand as={Link} to='/'>
           Cthulhuflix
         </Navbar.Brand>
